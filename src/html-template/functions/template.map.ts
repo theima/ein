a7696960ccfc0bdb1a681766db7ef42456a89b0f
Emanuel Eriksson <theima@users.noreply.github.com>
2018-03-23 19:49:47 +0100

    Template module (#4)
    * Refactoring names in preparation for restructuring
    
    * Moving over most template related functions and interfaces to template folder
    
    * Moving towards having properties as functions in render-data
    
    * Using attribute instead of property in template and renderer
    
    * Removing usage of TemplateString in RenderData
    
    * Removing templatvalidator and usage of properties in RenderData
    
    * Using partial with template map
    
    * Updating imports and separating the dynamic attributes from static in template

--- src/html-template/functions/template.map.ts
+++ src/html-template/functions/template.map.ts
@@ -5,26 +5,26 @@ import { getModel } from './get-model';
 import { BuiltIn } from '../types-and-interfaces/built-in';
 import { MapData } from '../types-and-interfaces/map-data';
 
-export function templateMap(maps: Dict<MapData>, template: Template): (m: object) => string {
-  return (model: object) => {
-    let parts = trimArray(template.split(BuiltIn.MapSeparator));
-    const initialValue: object | string | number | boolean = getModel(model, parts.shift() as string);
-    return parts.reduce((value: object | string | number | boolean, part: string, index: number) => {
-        const mapAndParameters = trimArray(part.split(BuiltIn.ParameterSeparator));
-        const mapName = mapAndParameters[0];
-        const mapData: MapData = get(maps, mapName);
-        const parameters = mapAndParameters.slice(1).map((param) => {
-          const result = parseTemplateParameter(model, param);
-          if (result === null) {
-            throw new Error('Could not parse parameter\'' + param + '\' for \'' + mapName + '\'');
+export function templateMap(maps: Dict<MapData>, template: Template) :(m: object) => string {
+    return (model: object) => {
+      let parts = trimArray(template.split(BuiltIn.MapSeparator));
+      const initialValue: object | string | number | boolean = getModel(model, parts.shift() as string);
+      return parts.reduce((value: object | string | number | boolean, part: string, index: number) => {
+          const mapAndParameters = trimArray(part.split(BuiltIn.ParameterSeparator));
+          const mapName = mapAndParameters[0];
+          const mapData: MapData = get(maps, mapName);
+          const parameters = mapAndParameters.slice(1).map((param) => {
+            const result = parseTemplateParameter(model, param);
+            if (result === null) {
+              throw new Error('Could not parse parameter\'' + param + '\' for \'' + mapName + '\'');
+            }
+            return result;
+          });
+          if (!mapData) {
+            throw new Error('Could not find map \'' + mapName + '\'.');
           }
-          return result;
-        });
-        if (!mapData) {
-          throw new Error('Could not find map \'' + mapName + '\'.');
+          return mapData.map(value, ...parameters);
         }
-        return mapData.map(value, ...parameters);
-      }
-      , initialValue) + '';
-  };
+        , initialValue) + '';
+    };
 }
