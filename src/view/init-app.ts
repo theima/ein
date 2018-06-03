79ea2ac747a58af8d29074af854433481eb5c2f4
theima <theima@users.noreply.github.com>
2018-06-03 18:13:39 +0200

    Fixing peer dependency, and the mapping for renderer

--- src/view/init-app.ts
+++ src/view/init-app.ts
@@ -1,6 +1,7 @@
 import { snabbdomRenderer } from '../html-renderer/functions/snabbdom-renderer';
 import { rootElementMap } from './functions/root-element.map';
 import { NodeAsync } from '../node-async';
+import { map } from 'rxjs/operators';
 
 import { HtmlElementData } from '../html-template/types-and-interfaces/html-element-data';
 import { HtmlNodeElementData } from '../html-template/types-and-interfaces/html-node-element-data';
@@ -9,9 +10,9 @@ import { MapData } from '../html-template';
 
 export function initApp(target: string, node: NodeAsync<object>, viewName: string, elements: Array<HtmlElementData | HtmlNodeElementData>, maps: MapData[]): void {
   let getElement = createTemplates(elements, maps);
-  const map = rootElementMap(getElement, viewName, node);
+  const elementMap = rootElementMap(getElement, viewName, node);
   const e = document.getElementById(target);
   if (e) {
-    snabbdomRenderer(e, (node as any).map(map));
+    snabbdomRenderer(e, (node as any).pipe(map(elementMap)));
   }
 }
