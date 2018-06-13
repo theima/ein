0f468a1df07c4aab02058d99022631550c528bec
theima <theima@users.noreply.github.com>
2018-06-13 20:34:47 +0200

    Partial for creating url middleware

--- src/state-router/create-states.ts
+++ src/state-router/create-states.ts
@@ -1,7 +1,7 @@
 import { createRouterMiddleware } from './create-router-middleware';
 import { StateConfig } from './types-and-interfaces/state.config';
 import { PathConfig } from './types-and-interfaces/path.config';
-import { createUrlMiddleware } from './create-url-middleware';
+import { urlMiddleware } from './url-middleware';
 import { pushUrl } from './functions/push-url';
 import { RuleConfig } from './types-and-interfaces/rule.config';
 import { StateDescriptor } from './types-and-interfaces/state.descriptor';
@@ -18,6 +18,7 @@ import { TransitionAction } from './types-and-interfaces/transition.action';
 import { StateAction } from './types-and-interfaces/state-action';
 import { Action, Middleware } from '../model';
 import { arrayToDict, Dict } from '../core';
+import { partial } from '../core/functions/partial';
 
 export function createStates(config: Array<RuleConfig | StateConfig>): { middleware: Middleware };
 export function createStates(config: Array<RuleConfig | StateConfig & PathConfig>): any;
@@ -31,7 +32,7 @@ export function createStates(config: Array<RuleConfig | StateConfig>): { middlew
   const pathConfig: PathConfig[] = stateConfig as any;
   if (pathConfig.length > 0 && pathConfig[0].path !== undefined) {
     const paths: Dict<PathConfig> = arrayToDict('name', pathConfig);
-    result.urlMiddleware = createUrlMiddleware(paths, pushUrl);
+    result.urlMiddleware = partial(urlMiddleware,paths, pushUrl);
     actions = popActions(pathConfig);
   } else {
     const defaultState: State = {
