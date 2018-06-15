d6c4e5d9251b68c43bc88a91675e2ebf86a9e071
theima <theima@users.noreply.github.com>
2018-06-15 23:57:08 +0200

    Using dict for elements in parser

--- src/html-template/functions/make-map.ts
+++
@@ -1,9 +0,0 @@
-//tslint:disable
-export function makeMap(str: string) {
-  let obj = {};
-  let items = str.split(',');
-  for (let i: number = 0; i < items.length; i++) {
-    obj[items[i]] = true;
-  }
-  return obj;
-}
