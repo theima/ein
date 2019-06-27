f7d1e470a72f9fcacb633523474bc7493c12377d
theima <theima@users.noreply.github.com>
2019-06-27 16:24:13 +0200

    Definig Value correctly

--- src/core/types-and-interfaces/value/value.ts
+++ src/core/types-and-interfaces/value/value.ts
@@ -1,1 +1,3 @@
-export type Value = object | string | number | boolean;
+type BaseValue = object | string | number | boolean;
+interface ValueObject { [prop: string]: BaseValue | ValueObject | Array<BaseValue | ValueObject>; }
+export type Value = BaseValue | ValueObject | Array<BaseValue | ValueObject>;
