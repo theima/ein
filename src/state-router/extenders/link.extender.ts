6fd63ab35765dd93a7575bcbbe30e7e872005f7e
theima <theima@users.noreply.github.com>
2019-06-29 15:26:51 +0200

    Registring eventlisteners correctly in link extender

--- src/state-router/extenders/link.extender.ts
+++ src/state-router/extenders/link.extender.ts
@@ -7,29 +7,33 @@ import { extender } from '../../html-renderer/functions/extender';
 
 export function linkExtender(configs: PathConfig[], postAction: (action: Action) => void): ExtenderDescriptor {
   return extender(BuiltIn.Link, (element: Element) => {
-    const update = () => {
-      const link: string = element.getAttribute(BuiltIn.Link) || '';
-      let action: Action;
-
-      const parts = link.split('?');
-      const path = parts[0];
-      const query = parts.length > 1 ? parts[1] : '';
-      action = pathToAction(configs, path, query);
-
-      const isLink = element.tagName === 'A';
-      if (isLink) {
-        element.setAttribute('href', link);
-      }
-      const listener = (event: MouseEvent) => {
-        postAction(action);
+    const elementIsLink = element.tagName === 'A';
+    let action: Action;
+    const listener = (event: MouseEvent) => {
+        if (action) {
+          postAction(action);
+        }
         if (event.button === 0 && !event.metaKey && !event.ctrlKey) {
           event.preventDefault();
         }
       };
-      element.addEventListener('click', listener as any);
+    element.addEventListener('click', listener as any);
+    const update = () => {
+      const link: string = element.getAttribute(BuiltIn.Link) || '';
+      const parts = link.split('?');
+      const path = parts[0];
+      const query = parts.length > 1 ? parts[1] : '';
+      action = pathToAction(configs, path, query);
+      if (elementIsLink) {
+        element.setAttribute('href', link);
+      }
+    };
+    const onBeforeDestroy = () => {
+      element.removeEventListener('click', listener as any);
     };
     return {
-      update
+      update,
+      onBeforeDestroy
     };
   });
 
