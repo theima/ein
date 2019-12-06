4cc5021ce641bf8ee95acc8475db297ddf0c7191
theima <theima@users.noreply.github.com>
2019-12-06 16:57:54 +0100

    Composing with connectNodeModifier

--- src/view/functions/apply-dependant-modifiers-temp.ts
+++ src/view/functions/apply-dependant-modifiers-temp.ts
@@ -1,14 +1,10 @@
-import { partial, Value } from '../../core';
+import { Value } from '../../core';
 import { compose } from '../../core/functions/compose';
-import { getArrayElement } from '../../core/functions/get-array-element';
 import { ModelToString } from '../../core/types-and-interfaces/model-to-string';
 import { NodeAsync } from '../../node-async';
-import { BuiltIn } from '../types-and-interfaces/built-in';
-import { DynamicProperty } from '../types-and-interfaces/dynamic-property';
 import { ModelToElementOrNull } from '../types-and-interfaces/elements/model-to-element-or-null';
 import { ModelToElements } from '../types-and-interfaces/elements/model-to-elements';
 import { Modifier } from '../types-and-interfaces/modifier';
-import { Property } from '../types-and-interfaces/property';
 import { FilledSlot } from '../types-and-interfaces/slots/filled.slot';
 import { MappedSlot } from '../types-and-interfaces/slots/mapped.slot';
 import { FilledElementTemplate } from '../types-and-interfaces/templates/filled.element-template';
@@ -22,21 +18,12 @@ export function applyDependantModifiersTemp(getId: () => string,
                                             node: NodeAsync<Value>,
                                             template: FilledElementTemplate) {
   const viewId = getId();
-  const attrs = template.properties.map(a => {
-    return { ...a, name: a.name.toLowerCase() };
-  });
-  const getAttr = partial(getArrayElement as any, 'name', attrs);
-  const connectAttr: Property | DynamicProperty = getAttr(BuiltIn.Connect) as any;
   const last = (node: NodeAsync<Value>, template: FilledElementTemplate) => {
     return createElementMap(template, viewId, contentMap);
   };
-  const modifiers: Modifier[] = [streamModifier, connectActionsModifier];
-  const initiated= modifiers.map(m => m(viewId, contentMap));
+  const modifiers: Modifier[] = [connectNodeModifier, streamModifier, connectActionsModifier];
+  const initiated = modifiers.map(m => m(viewId, contentMap));
   const composed = compose(last, ...initiated);
-
-  if (connectAttr) {
-    return connectNodeModifier(viewId, contentMap)(last)(node, template);
-  }
 
   return composed(node, template);
 
--- src/view/functions/modifiers/connect-node.modifier.ts
+++ src/view/functions/modifiers/connect-node.modifier.ts
@@ -1,23 +1,16 @@
 import { Observable, ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Action, Value } from '../../../core';
-import { getArrayElement } from '../../../core/functions/get-array-element';
+import { Value } from '../../../core';
 import { ModelToString } from '../../../core/types-and-interfaces/model-to-string';
 import { NodeAsync } from '../../../node-async';
 import { BuiltIn } from '../../types-and-interfaces/built-in';
 import { LiveElement } from '../../types-and-interfaces/elements/live.element';
 import { ModelToElementOrNull } from '../../types-and-interfaces/elements/model-to-element-or-null';
 import { ModelToElements } from '../../types-and-interfaces/elements/model-to-elements';
-import { Property } from '../../types-and-interfaces/property';
-import { Select } from '../../types-and-interfaces/select';
 import { FilledSlot } from '../../types-and-interfaces/slots/filled.slot';
 import { MappedSlot } from '../../types-and-interfaces/slots/mapped.slot';
 import { FilledElementTemplate } from '../../types-and-interfaces/templates/filled.element-template';
-import { createApplyActionHandlers } from '../create-apply-action-handlers';
-import { mapContent } from '../element-map/map-content';
 import { getProperty } from '../get-property';
-import { selectActions } from '../select-actions';
-import { claimProperty } from './claim-property';
 
 export function connectNodeModifier(viewId: string,
                                     contentMap: (e: FilledElementTemplate | ModelToString | FilledSlot) => ModelToElementOrNull | ModelToElements | ModelToString | MappedSlot) {
@@ -25,22 +18,9 @@ export function connectNodeModifier(viewId: string,
     return (node: NodeAsync<Value>, template: FilledElementTemplate) => {
       const connectProperty = getProperty(BuiltIn.Connect, template);
       if (connectProperty) {
-        const actionAttr = getArrayElement('name', template.properties, BuiltIn.Actions) as Property;
-        const actions: (select: Select) => Observable<Action> = actionAttr.value as any;
-        let selectWithStream = selectActions(actions);
-        const applyActionHandlers = createApplyActionHandlers(selectWithStream.selects);
-        node.next(selectWithStream.stream);
-        template = claimProperty(BuiltIn.Connect, template);
-        template = claimProperty(BuiltIn.ConnectActions, template);
-        const mappedContent = template.content.map(contentMap);
-        const elements = (m: any) => {
-          let content = mapContent('', mappedContent, m, m);
-          return {
-            name: template.name,
-            id: viewId,
-            properties: [],
-            content: applyActionHandlers(content)
-          };
+        const result = next(node, template);
+        const elementMap = (m: any) => {
+          return result(m, m) as any;
         };
         const nodeStream: Observable<any> = node as any;
         const updates = new ReplaySubject<Element>(1);
@@ -53,7 +33,7 @@ export function connectNodeModifier(viewId: string,
           () => {
             updates.complete();
           });
-        const stream = updates.pipe(map(elements));
+        const stream = updates.pipe(map(elementMap));
         const willBeDestroyed = () => {
           subscription.unsubscribe();
           updates.complete();
