18694522e9f9e64711b69db343ad827bc657eacf
theima <theima@users.noreply.github.com>
2020-09-30 19:38:43 +0200

    Updating deps, cleaning up partial typings, finally

--- src/core/functions/partial.ts
+++ src/core/functions/partial.ts
@@ -1,41 +1,5 @@
-export function partial<A, B>(func: (a: A) => B, a: A): () => B;
-
-export function partial<A, B, C>(func: (a: A, b: B) => C, a: A): (b: B) => C;
-
-export function partial<A, B, C, D>(func: (a: A, b: B, c: C) => D, a: A): (b: B, c: C) => D;
-export function partial<A, B, C, D>(func: (a: A, b: B, c: C) => D, a: A, b: B): (c: C) => D;
-
-export function partial<A, B, C, D, E>(func: (a: A, b: B, c: C, d: D) => E, a: A): (b: B, c: C, d: D) => E;
-export function partial<A, B, C, D, E>(func: (a: A, b: B, c: C, d: D) => E, a: A, b: B): (c: C, d: D) => E;
-export function partial<A, B, C, D, E>(func: (a: A, b: B, c: C, d: D) => E, a: A, b: B, c: C): (d: D) => E;
-
-export function partial<A, B, C, D, E, F>(func: (a: A, b: B, c: C, d: D, e: E) => F, a: A): (b: B, c: C, d: D, e: E) => F;
-export function partial<A, B, C, D, E, F>(func: (a: A, b: B, c: C, d: D, e: E) => F, a: A, b: B): (c: C, d: D, e: E) => F;
-export function partial<A, B, C, D, E, F>(func: (a: A, b: B, c: C, d: D, e: E) => F, a: A, b: B, c: C): (d: D, e: E) => F;
-export function partial<A, B, C, D, E, F>(func: (a: A, b: B, c: C, d: D, e: E) => F, a: A, b: B, c: C, d: D): (e: E) => F;
-
-export function partial<A, B, C, D, E, F, G>(func: (a: A, b: B, c: C, d: D, e: E, f: F) => G, a: A): (b: B, c: C, d: D, e: E, f: F) => G;
-export function partial<A, B, C, D, E, F, G>(func: (a: A, b: B, c: C, d: D, e: E, f: F) => G, a: A, b: B): (c: C, d: D, e: E, f: F) => G;
-export function partial<A, B, C, D, E, F, G>(func: (a: A, b: B, c: C, d: D, e: E, f: F) => G, a: A, b: B, c: C): (d: D, e: E, f: F) => G;
-export function partial<A, B, C, D, E, F, G>(func: (a: A, b: B, c: C, d: D, e: E, f: F) => G, a: A, b: B, c: C, d: D): (e: E, f: F) => G;
-export function partial<A, B, C, D, E, F, G>(func: (a: A, b: B, c: C, d: D, e: E, f: F) => G, a: A, b: B, c: C, d: D, e: E): (f: F) => G;
-
-export function partial<A, B, C, D, E, F, G, H>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H, a: A): (b: B, c: C, d: D, e: E, f: F, g: G) => H;
-export function partial<A, B, C, D, E, F, G, H>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H, a: A, b: B): (c: C, d: D, e: E, f: F, g: G) => H;
-export function partial<A, B, C, D, E, F, G, H>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H, a: A, b: B, c: C): (d: D, e: E, f: F, g: G) => H;
-export function partial<A, B, C, D, E, F, G, H>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H, a: A, b: B, c: C, d: D): (e: E, f: F, g: G) => H;
-export function partial<A, B, C, D, E, F, G, H>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H, a: A, b: B, c: C, d: D, e: E): (f: F, g: G) => H;
-export function partial<A, B, C, D, E, F, G, H>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H, a: A, b: B, c: C, d: D, e: E, f: F): (g: G) => H;
-
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A): (b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I;
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A, b: B): (c: C, d: D, e: E, f: F, g: G, h: H) => I;
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A, b: B, c: C): (d: D, e: E, f: F, g: G, h: H) => I;
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A, b: B, c: C, d: D): (e: E, f: F, g: G, h: H) => I;
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A, b: B, c: C, d: D, e: E): (f: F, g: G, h: H) => I;
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A, b: B, c: C, d: D, e: E, f: F): (g: G, h: H) => I;
-export function partial<A, B, C, D, E, F, G, H, I>(func: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I, a: A, b: B, c: C, d: D, e: E, f: F, g: G): (h: H) => I;
-export function partial(func: (...params: any[]) => any, ...partialParams: any[]): (...params: any[]) => any {
-  return (...params) => {
+export function partial<A extends readonly any[], B extends readonly any[], C>(func: (...args: [...A, ...B]) => C, ...partialParams: A): (...params: B) => C {
+  return (...params: B) => {
     return func(...partialParams, ...params);
   };
 }
