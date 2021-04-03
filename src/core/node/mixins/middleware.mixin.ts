8437c38a95bdb219fc8c748a360c4cc75d0f737b
theima <theima@users.noreply.github.com>
2021-04-03 16:19:19 +0200

    adding lining fix for unused imports

--- src/core/node/mixins/middleware.mixin.ts
+++ src/core/node/mixins/middleware.mixin.ts
@@ -7,42 +7,42 @@ import { TriggerMiddleWare } from '../types-and-interfaces/trigger-middleware';
 import { Update } from '../types-and-interfaces/update';
 
 export function middlewareMixin<
-  T,
-  NBase extends NodeConstructor<NodeBehaviorSubject<T>>
+    T,
+    NBase extends NodeConstructor<NodeBehaviorSubject<T>>
 >(
-  middleware: Middleware[],
-  triggerMiddleware: TriggerMiddleWare[],
-  node: NBase
+    middleware: Middleware[],
+    triggerMiddleware: TriggerMiddleWare[],
+    node: NBase
 ): NBase {
-  return class extends node {
-    constructor(...rest: any[]) {
-      super(...rest);
-      if (middleware.length > 0) {
-        this.mapAction = chainMiddleware(
-          this as any,
-          this.mapAction,
-          middleware
-        );
-      }
-      if (triggerMiddleware.length > 0) {
-        this.mapTriggeredAction = (update: Update<T>) => {
-          let model: T = update.model
-          const tempWrapped = (action: Action) => {
-            model = this.mapTriggeredAction(update)
-            return action;
-          }
-          const tempChained = chainMiddleware(
-            this as any,
-            tempWrapped,
-            triggerMiddleware
-          );
-          if (update.action) {
-            tempChained(update.action);
-          }
-          return model;
-        }
-      }
+    return class extends node {
+        constructor(...rest: any[]) {
+            super(...rest);
+            if (middleware.length > 0) {
+                this.mapAction = chainMiddleware(
+                    this as any,
+                    this.mapAction,
+                    middleware
+                );
+            }
+            if (triggerMiddleware.length > 0) {
+                this.mapTriggeredAction = (update: Update<T>) => {
+                    let model: T = update.model
+                    const tempWrapped = (action: Action) => {
+                        model = this.mapTriggeredAction(update)
+                        return action;
+                    }
+                    const tempChained = chainMiddleware(
+                        this as any,
+                        tempWrapped,
+                        triggerMiddleware
+                    );
+                    if (update.action) {
+                        tempChained(update.action);
+                    }
+                    return model;
+                }
+            }
 
+        }
     }
-  }
 }
